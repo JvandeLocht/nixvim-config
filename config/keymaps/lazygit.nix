fda935bdf2d06c2762eaf72bcf55b7a2c8e9bc3a
Jan van de Locht <jan@vandelocht.uk>
2024-07-22 12:50:23 +0200

    Feat: Added and configured LazyGit.

---
+++ config/keymaps/lazygit.nix
@@ -0,0 +1,11 @@
+# Navigate tabs
+# https://github.com/AstroNvim/AstroNvim/blob/v4.7.7/lua/astronvim/plugins/_astrocore_mappings.lua#L103-L104
+[
+  {
+    mode = "n";
+    key = "<leader>gg";
+
+    action = "LazyGit";
+    options.desc = "Open LazyGit";
+  }
+]
